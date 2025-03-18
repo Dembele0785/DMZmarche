85275b30472a7ed82d0182296690f9004461f1c2
ziedembele0785 <demebelebtssio@gmail.com>
2025-03-18 17:54:42 +0100

    comit marche

--- src/app/services/cours.service.ts
+++ src/app/services/cours.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Cours} from '../model/cours.model';
+import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,11 @@ import {Cours} from '../model/cours.model';
 export class CoursService {
   private apiUrl = 'http://localhost:8080/cours';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,private auth: AuthService) {
   }
 
   getCourss(): Observable<Cours[]> {
-    let courss = this.http.get<Cours[]>(this.apiUrl);
+    let courss = this.http.get<Cours[]>(this.apiUrl,{headers:this.auth.headers});
     return courss;
   }
 }
